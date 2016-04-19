5f151f49acba0759fe945305ce22b409ae11a849
Arda Özkal <ardaozkal@users.noreply.github.com>
2016-04-19 18:08:03 +0300

    Update data.php

--- data.php
+++ data.php
@@ -1,12 +1,39 @@
 <?php
+$test = false;
+$mile = isset($_GET["m"]);
+if ($test)
+{
+echo "<s>".rand (0,120)."</s><l>".rand (0,120)."</l><e>1</e>";
+}
+else 
+{
 $jsonIterator = new RecursiveIteratorIterator(
     new RecursiveArrayIterator(json_decode(file_get_contents("http://localhost:25555/api/ets2/telemetry"), TRUE)),
     RecursiveIteratorIterator::SELF_FIRST);
-
 foreach ($jsonIterator as $key => $val) {
     if(!is_array($val)) {
-        if ($key == $_GET["key"])
-        echo "<val>".$val."</val>";
+        if ($mile)
+        {
+        if ($key == "speed")
+        echo "<s>".round($val*0.621371)."</s>";
+
+        if ($key == "speedLimit")
+        echo "<l>".round($val*0.621371)."</l>";
+
+        if ($key == "engineOn")
+        echo "<e>".$val."</e>";
+        }
+        else {
+        if ($key == "speed")
+        echo "<s>".round($val)."</s>";
+
+        if ($key == "speedLimit")
+        echo "<l>".round($val)."</l>";
+
+        if ($key == "engineOn")
+        echo "<e>".$val."</e>";
+        }
     }
+}
 }
 ?>
