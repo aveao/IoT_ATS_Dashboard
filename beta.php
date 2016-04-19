8b110e5f7331cc232c2d9880f63f95abbf1be44e
Arda Özkal <ardaozkal@users.noreply.github.com>
2016-04-19 16:49:23 +0300

    Attempt to make Web calls faster

---
+++ beta.php
@@ -0,0 +1,14 @@
+<?php
+$jsonIterator = new RecursiveIteratorIterator(
+    new RecursiveArrayIterator(json_decode(file_get_contents("http://localhost:25555/api/ets2/telemetry"), TRUE)),
+    RecursiveIteratorIterator::SELF_FIRST);
+foreach ($jsonIterator as $key => $val) {
+    if(!is_array($val)) {
+        if ($key == "speed")
+        echo "<speed>".$val."</speed>";
+
+        if ($key == "speedLimit")
+        echo "<SpeedLimit>".$val."</SpeedLimit>";
+    }
+}
+?>
